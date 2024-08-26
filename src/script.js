c86b4c5b886080eb9e05aa6d879f5273be17be63
Marlon <mrlonbarretoborges@gmail.com>
2024-08-26 17:14:08 -0300

    hjhjh

--- src/script.js
+++ src/script.js
@@ -12,7 +12,7 @@ btn.addEventListener('click', ()=>{
 
     async function delay(){
         
-            for(let i = 1; i > 0; i++){
+            for(let i = 0; i < 0; i++){
                 setTimeout(()=> {
         
                 const p = document.createElement("p")
